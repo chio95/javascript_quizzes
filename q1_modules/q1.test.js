ff72138dbc0137b3ea07aa9bd45852b78f643ab2
yena <chlwl0406@daum.net>
2017-07-18 02:25:25 +0000

    quiz finish

--- q1_modules/q1.test.js
+++ q1_modules/q1.test.js
@@ -1,15 +1,14 @@
+// 여기에 'FOO' 와 'BAR' 문자열을 임포트하세요
+// 또한 아래의 bazString을 실제로 임포트할 수 있도록 올바른 위치에 파일을 만드세요.
+import { bazString } from './baz/bazFile';
+import barString from './bar';
+import { fooString } from './foo';
 
-  // 여기에 'FOO' 와 'BAR' 문자열을 임포트하세요
-  // 또한 아래의 bazString을 실제로 임포트할 수 있도록 올바른 위치에 파일을 만드세요.
-  import { bazString } from './baz/bazFile';
-  import barString from './bar';
-  import { fooString } from './foo';
+const result = fooString + barString + bazString;
 
-  const result = fooString + barString + bazString;
-  
-  const expectedResult = 'FOOBARBAZ';
-  
- // 아래의 함수가 위의 값이 서로 같은지를 비교하고 다르다면 테스트를 실패시킵니다.
- describe('Q1_Modules', () => test('Answer', () => {
-   expect(result).toBe(expectedResult);
- }));
+const expectedResult = 'FOOBARBAZ';
+
+// 아래의 함수가 위의 값이 서로 같은지를 비교하고 다르다면 테스트를 실패시킵니다.
+describe('Q1_Modules', () => test('Answer', () => {
+  expect(result).toBe(expectedResult);
+}));
